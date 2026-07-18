64aac8f05576b2a0517664b510d737011dc29d0a
Darsh <yadavabheesht@gmail.com>
2026-07-18 16:48:42 +0530

    p2

---
+++ patterns/p2.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    for(int i=0;i<5;i++){
+        for(int x=0;x<=i;x++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
