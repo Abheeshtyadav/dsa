0fe72769f339b94f910949f4912c783adb043e6e
Darsh <yadavabheesht@gmail.com>
2026-07-18 20:36:33 +0530

    p7

---
+++ patterns/p7.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    for(int i=0;i<5;i++){
+        for(int x=3;x>=i;x--)
+        cout << " ";
+        for(int y=1;y<=i;y++)
+        cout << "e";
+        for(int z=0;z<=i;z++)
+        cout << "e";
+        
+        cout << endl;
+    }
+}
