6a63ed65a51c1ffe3ff6055be5abba5960d08d3f
Darsh <yadavabheesht@gmail.com>
2026-07-18 19:31:32 +0530

    p6

---
+++ patterns/p6.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    for(int i = 1;i<=5;i++){
+        for(int x=5;x>=i;x--){
+            cout << 5-x+1;
+        }
+        cout << endl;
+    }
+}
