adf786ecac8682e67ab7a5c4a989ce548c6c5127
Darsh <yadavabheesht@gmail.com>
2026-07-18 16:44:44 +0530

    p1

---
+++ patterns/p1.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    for(int i=0;i<5;i++){
+        for(int x=0;x<5;x++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
