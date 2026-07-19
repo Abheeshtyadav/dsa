e8863bfa19b19bd21721911fa6dd40725c5f308b
Darsh <yadavabheesht@gmail.com>
2026-07-19 13:19:59 +0530

    p14

---
+++ patterns/p14.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    for(int i=0;i<5;i++){
+        char c = 'A';
+        for(int x=0;x<=i;x++){
+            cout << c;
+            c++;
+        }
+        cout << endl;
+    }
+}
