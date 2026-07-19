47a9dc36901bf625637af2c3e710595b287e4473
Darsh <yadavabheesht@gmail.com>
2026-07-19 13:17:28 +0530

    p13

---
+++ patterns/p13.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main(){
+    int a=1;
+    for(int i=0;i<5;i++){
+        for(int x=0;x<=i;x++){
+            cout << a << " ";
+            a++;
+        }
+        cout << endl;
+
+    }
+}
