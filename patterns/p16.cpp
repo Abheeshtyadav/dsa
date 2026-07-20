55a5ba73ab16c8150d321d1d910b9d207399e8fa
Darsh <yadavabheesht@gmail.com>
2026-07-20 13:32:46 +0530

    p16

---
+++ patterns/p16.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    for(int i =0 ;i<5;i++){
+        for(int x = 0;x<=i ; x++){
+            cout << char(i+65);
+        }
+        cout << endl;
+    }
+}
