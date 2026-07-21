7006079bfb9a244f9439ce52faba2a431eef2fd1
Darsh <yadavabheesht@gmail.com>
2026-07-21 23:27:09 +0530

    pairs

---
+++ STL/pairs.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    pair<int , int> p = {1,3};
+    vector<int> v(5);
+    v.push_back(1);
+    v.push_back(4);
+    for(int i:v){
+        cout << i << " ";
+    }
+
+}
