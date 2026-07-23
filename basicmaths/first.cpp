e0a9e0759adf1938af088a3cfd4a7d68ca5b5b97
Darsh <yadavabheesht@gmail.com>
2026-07-24 02:51:28 +0530

    u

--- basicmaths/first.cpp
+++ basicmaths/first.cpp
@@ -1,10 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int w=235345;
-    while(w>0){
-        cout << w%10 << endl;
-        w=w/10;
+int countt(){
+    int n;
+    cin >> n;
+    int counter=0;
+    int las;
+    while(n>0){
+        las=n%10;
+        counter++;
+        n=n/10;
     }
+    return counter;
 }
+
+int main(){
+
+    cout << countt();
+    
+}
+
