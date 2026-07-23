75e5dd428da68e52584f11750e747c703b628896
Darsh <yadavabheesht@gmail.com>
2026-07-24 01:40:15 +0530

    pairs

--- STL/pairs.cpp
+++ STL/pairs.cpp
@@ -3,11 +3,17 @@ using namespace std;
 
 int main(){
     pair<int , int> p = {1,3};
-    vector<int> v(5);
+    vector<int> v(1,100);
     v.push_back(1);
     v.push_back(4);
-    for(int i:v){
-        cout << i << " ";
+    for(vector<int>::iterator it=v.begin();it!=v.end();it++){
+    
+    cout << *it << endl;
+    }
+    cout << "-------------" << endl;
+    sort(v.begin(),v.end(),greater<int>());
+    for(vector<int>::iterator it=v.begin();it!=v.end();it++){
+        cout << *it << endl;
     }
 
 }
