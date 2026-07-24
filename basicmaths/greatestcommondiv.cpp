e245b11cb2fdf062a6b9612562fb2fe9881aa629
Darsh <yadavabheesht@gmail.com>
2026-07-24 19:30:01 +0530

    u

---
+++ basicmaths/greatestcommondiv.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int brute_force(int n,int m){
+
+    int mini=min(n,m);
+    for(int i=mini;i>0;i--){
+        if(n%i==0 && m%i==0)
+        return i;
+    }
+
+
+    return 1;
+}
+
+int main(){
+    int n,m;
+    cin >> n;
+    cin >> m;
+    cout << brute_force(n,m) << endl;
+
+}
