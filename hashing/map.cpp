3e8056c41f05cc2c5d4222b5157a6dc9004c70d0
Darsh <yadavabheesht@gmail.com>
2026-07-28 12:58:53 +0530

    u

--- hashing/map.cpp
+++ hashing/map.cpp
@@ -1,9 +1,10 @@
 //using map it is more efficient
 //it stores all in sorted manner
+//first we should try unordered_map if it givel tle then we should try map
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+void number_hashing_usingmap(){
     int arr[10] = {3,3,4,5,4,6,7,6,3,4};
     map<int,int> mpp;
     for(int i = 0;i<10;i++){
@@ -11,4 +12,20 @@ int main(){
     }
 
     cout << mpp[4];
+}
+
+void character_hashing_usingmap(){
+    string a = "sdgsfgsss";
+    map<char,int> mpp;
+    for(int i = 0;i<a.length();i++){
+        mpp[a[i]]++;
+    }
+    cout << mpp['s'];
+
+}
+
+
+int main(){
+   /*  number_hashing_usingmap(); */
+    character_hashing_usingmap();
 }
